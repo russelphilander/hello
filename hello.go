221ed2833e39c2569461493d92232a49f5fbd8ed
Russel Philander <russel.philander@gmail.com>
2014-08-12 17:26:12 +0200

    Testing GO with Mongo

--- hello.go
+++ hello.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
-func main(){
-	fmt.Println("Hi, this is my second program")
+type Person struct {
+	Name    string `bson: "name"`
+	Surname string `bson: "surname"`
+	Age     int    `bson: "age"`
+}
+
+type Test struct {
+	school string
+	course string
+	mark   string
+}
+
+func main() {
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("mydb").C("Person")
+
+	/*
+		err = c.Insert(&Person{Name: "Pienut", Surname: "Butter"})
+
+		if err != nil {
+			panic(err)
+		}
+	*/
+
+	result := new(Person) //bson.M{}
+	iter := c.Find(bson.M{}).Iter()
+
+	for iter.Next(result) {
+		fmt.Printf("Result: %v\n", result)
+
+		if iter.Err() != nil {
+			panic(iter.Err())
+		}
+	}
+
+	fmt.Println(result)
+
+	testing := new(Test)
+	testing.school = "Laerskool Darling"
+	testing.course = "Maths"
+	testing.mark = "87"
+
+	fmt.Println(testing)
 }
